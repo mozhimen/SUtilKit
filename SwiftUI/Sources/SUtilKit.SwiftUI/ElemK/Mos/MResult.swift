62fc89cbcb6af6d2a33df6f4006f1b324223434c
mozhimen <58215586+mozhimen@users.noreply.github.com>
2025-07-14 18:54:17 +0800

    250714

--- SwiftUI/Sources/SUtilKit.SwiftUI/ElemK/Mos/MResult.swift
+++ SwiftUI/Sources/SUtilKit.SwiftUI/ElemK/Mos/MResult.swift
@@ -5,14 +5,23 @@
 //  Created by Taiyou on 2025/7/9.
 //
 
-public struct MResultIST<T> {
-    let code: Int?
-    let msg: String?
-    let t: T
+public struct MResultIST<T:Decodable> : Decodable {
+    public let code: Int?
+    public let msg: String?
+    public let t: T?
 
-    public init(code: Int?, msg: String?, t: T) {
+    public init(code: Int?, msg: String?, t: T?) {
         self.code = code
         self.msg = msg
         self.t = t
+    }
+    
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let value = try? container.decode(MResultIST.self) {
+            self = value
+        } else {
+            self = MResultIST(code: 0, msg: nil, t: nil)
+        }
     }
 }
