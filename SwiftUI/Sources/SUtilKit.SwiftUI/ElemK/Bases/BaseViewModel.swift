f13665c0fac4386c1a4d8d6d8a4ba3d48eef7436
mozhimen <58215586+mozhimen@users.noreply.github.com>
2025-07-23 18:39:54 +0800

    250723

---
+++ SwiftUI/Sources/SUtilKit.SwiftUI/ElemK/Bases/BaseViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  BaseViewModel.swift
+//  SUtilKit.SwiftUI
+//
+//  Created by Taiyou on 2025/7/23.
+//
+import SwiftUI
+import Combine
+open class BaseViewModel:ObservableObject{
+    var viewModelScope = Set<AnyCancellable>()
+}
--- SwiftUI/Sources/SUtilKit.SwiftUI/Utilk/UtilKCollection.swift
+++ SwiftUI/Sources/SUtilKit.SwiftUI/Utilk/UtilKCollection.swift
@@ -9,10 +9,18 @@ public extension Array {
     func isNotEmpty() -> Bool {
         return UtilKCollection.isNotEmpty(collection: self)
     }
+    
+    func isNullOrEmpty() -> Bool {
+        return UtilKCollection.isNullOrEmpty(collection: self)
+    }
 }
 
 public final class UtilKCollection {
     public static func isNotEmpty<T>(collection: [T]) -> Bool {
         return !collection.isEmpty
+    }
+    
+    public static func isNullOrEmpty<T>(collection: [T]?) -> Bool {
+        return collection == nil || collection?.isEmpty == true
     }
 }
